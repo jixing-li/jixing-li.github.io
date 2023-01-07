91dc2a36936fe635250dc44fdcbee5720358d2ff
Jixing Li <jixing.us@gmail.com>
2023-01-07 14:41:24 +0800

    Delete comments.js

--- comments.js
+++
@@ -1,8 +0,0 @@
-var post= document.getElementById("post");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment").value; 
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("complaint").appendChild(li);
- });
